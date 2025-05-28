ab6a8ce529862cb6ebc95a93066396a685ed0dea
shadowpercifal <percifal@shpw.ru>
2025-05-28 15:39:43 +0300

    i love VS

--- Features/Objects/SchematicObject.cs
+++ Features/Objects/SchematicObject.cs
@@ -172,8 +172,8 @@ public class SchematicObject : MonoBehaviour
 		NetworkServer.Spawn(gameObject);
 		gameObject.transform.SetParent(parentTransform);
 		// _attachedBlocks.Add(gameObject);
-        ObjectFromId.Add(block.ObjectId, gameObject.transform);
-
+		ObjectFromId.Add(block.ObjectId, gameObject.transform);
+		
 		if (block.BlockType != BlockType.Light && TryGetAnimatorController(block.AnimatorName, out RuntimeAnimatorController animatorController))
 			_animators.Add(gameObject, animatorController);
 
